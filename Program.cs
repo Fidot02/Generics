d66312b689526c70708c8ed9dc659a193e23803f
Fidot02 <emekafelixabojei@gmail.com>
2025-04-14 00:32:36 +0100

    generics upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Storing and Retriving Items using Generic
+
+using Generics;
+
+Stotage<string> stotage = new Stotage<string>();
+
+stotage.StoreItem("Boy");
+
+stotage.RetrieveItem();
+Console.WriteLine();
+
+Stotage<int> intStotage = new Stotage<int>();
+
+intStotage.StoreItem(30);
+
+intStotage.RetrieveItem();
+Console.WriteLine();
+
+Stotage<float> floatStotage = new Stotage<float>();
+floatStotage.StoreItem(12f);
+floatStotage.RetrieveItem();
+
+Console.ReadKey();
+
---
+++ Stotage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Stotage<T>
+    {
+        private T _item;
+
+        public void StoreItem(T item)
+        {
+            _item = item;
+            Console.WriteLine($"item has been stored");
+        }
+        public T RetrieveItem()
+        {  
+            Console.WriteLine($"item retrieved:{_item}");
+            return _item;
+        }
+    }
+
+}
